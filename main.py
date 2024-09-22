c61f8df7c5fa41db7fb9c2087325bbe7d5f21dcd
Zeraphim <diamantejc87@gmail.com>
2024-09-23 01:13:41 +0800

    added markdowns and descriptions per chart

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ from wordcloud import WordCloud
 from mpl_toolkits.mplot3d import Axes3D
 import squarify
 
-st.title('Data Visualization Fundamentals - Lesson 4')
-st.write('by JC Diamante')
+st.title('Data Visualization Using Streamlit')
+st.markdown('`by JC Diamante`')
+
+st.markdown("""    
+The purpose of this is to convert our previous Data Visualization lesson in [Google Colab Notebook](https://colab.research.google.com/drive/13hXacZrlV2WYvuhtrylJVa20MzQ8b_j_?usp=sharing) into a Streamlit Web Application.  
+  
+Also check the [GitHub Repository](https://github.com/Zeraphim/Data-Visualization-Fundamentals) for the source code
+""")
 
 # Data Generation (Dummy Data)
 
@@ -58,6 +64,17 @@ def bar_chart():
   plt.clf()
 
 st.header("Bar Chart Demo")
+st.markdown("""
+
+Ideal for comparing categorical data or displaying frequencies, bar charts offer a clear visual representation of values.  
+  
+**Use Cases:**  
+1. Comparing the sales figures of different products  
+2. Displaying the vote counts for each candidate in an election  
+3. Comparing the number of customers in different age groups.  
+4. Comparing the amount of website traffic from different sources.  
+
+""")
 bar_chart()
 
 # Line Chart using Matplotlib
@@ -74,6 +91,17 @@ def line_chart():
     plt.clf()
 
 st.header("Line Chart Demo")
+st.markdown("""
+            
+Perfect for illustrating trends over time, line charts connect data points to reveal patterns and fluctuations.  
+  
+**Use Cases:**  
+1. Tracking the fluctuation of stock prices over days, months, or years  
+2. Monitoring website traffic over time to identify peak hours and seasonal trends.  
+3. Visualizing sales figures over time to identify growth patterns and seasonal variations  
+4. Depicting temperature variations throughout the day, week, or year.  
+            
+""")
 line_chart()
 
 # Pie Chart using Matplotlib
@@ -87,7 +115,19 @@ def pie_chart():
     plt.clf()
 
 st.header("Pie Chart Demo")
+st.markdown("""
+
+Efficient for displaying parts of a whole, pie charts offer a simple way to understand proportions and percentages.  
+  
+**Use Cases:**  
+1. Depicting market share of different companies within an industry  
+2. Visualizing how a budget is allocated across different departments or expense categories  
+3. Presenting proportion of respondents who selected each option in a survey.  
+4. Showing the distribution of a population based on age, gender, or ethnicity  
+
+""")
 pie_chart()
+
 
 # Scatter Plot using Matplotlib
 def scatter_plot():
@@ -99,6 +139,17 @@ def scatter_plot():
     plt.clf()
 
 st.header("Scatter Plot Demo")
+st.markdown("""
+            
+Showcase relationships between two variables, identifying patterns and outliers through scattered data points.  
+  
+**Use cases:**  
+1. Analyzing relationship between inflation and unemployment  
+2. Studying the correlation between income and spending.  
+3. Investigating the correlation between blood pressure and heart rate  
+4. Studying the relationship between drug dosage and patient response.  
+
+""")
 scatter_plot()
 
 # Histogram using Matplotlib
@@ -114,6 +165,15 @@ def histogram():
     plt.clf()
 
 st.header("Histogram Demo")
+st.markdown("""
+
+Depict the distribution of a continuous variable, providing insights into the underlying data patterns.  
+  
+**Use cases:**  
+1. Exam Scores  
+2. Income Distribution  
+
+""")
 histogram()
 
 # Heatmap using Seaborn
@@ -131,6 +191,17 @@ def heatmap():
     plt.clf()
     
 st.header("Heatmap Demo")
+st.markdown("""
+
+Visualize complex data sets through color-coding, emphasizing variations and correlations in a matrix.  
+  
+**Use Cases:**  
+1. Tracking of user behavior on websites, where they click, scroll, and hover their mouse.  
+2. Tracking the effectiveness of marketing campaigns, where people are clicking on ads or landing pages.  
+3. Track stock prices, currency exchange rates, etc.  
+4. Track the spread of diseases, effectiveness of treatments, and other health-related data.  
+
+""")
 heatmap()
 
 # Box Plot using Seaborn
@@ -144,6 +215,16 @@ def box_plot():
     plt.clf()
 
 st.header("Box Plot Demo")
+st.markdown("""
+
+Unveil statistical summaries such as median, quartiles, and outliers, aiding in data distribution analysis.  
+  
+**Use Cases:**  
+1. Identifying outliers in data. Like in customer purchase history.  
+2. Comparing the distributions of two or more groups of data.  
+3. Understand the spread of data for making decisions about how to analyze or interpret the data. Like in the spread of customer ages in a dataset.  
+            
+""")
 box_plot()
 
 # Area Chart using Altair
@@ -163,6 +244,17 @@ def area_chart():
     st.altair_chart(area_chart, use_container_width=True)
 
 st.header("Area Chart Demo")
+st.markdown("""
+
+Similar to line charts but with the area under the line filled, these charts accentuate cumulative data patterns.  
+  
+**Use Cases:**  
+1. Tracking website traffic over time, it can highlight the peak traffic periods.  
+2. Visualizing sales or revenue trends  
+3. Representing market share changes  
+4. Displaying social media engagements.  
+
+""")
 area_chart()
 
 # Bubble Chart using Matplotlib
@@ -175,6 +267,16 @@ def bubble_chart():
     plt.clf()
 
 st.header("Bubble Chart Demo")
+st.markdown("""
+
+Enhance scatter plots by introducing a third dimension through varying bubble sizes, revealing additional insights.  
+  
+**Use Cases:**  
+1. Stock market analysis. Company's earnings (x-axis), market capitalization (y-axis), and trading volume (bubble size)  
+2. Population Demographics. Population of a country (x-axis), Life Expectancy (y-axis), Gross Domestic Product or GDP (Bubble Size)  
+3. Sales Data, Sales per region (x-axis), Profit Margin (y-axis), Number of units sold (bubble size)  
+
+""")
 bubble_chart()
 
 # Treemap using Squarify
@@ -193,6 +295,16 @@ def treemap():
     plt.clf()
 
 st.header("Treemap Demo")
+st.markdown("""
+
+Efficiently represent hierarchical data structures, breaking down categories into nested rectangles.  
+  
+**Use Cases:**  
+1. Analyze disk space usage, files and folders can be represented with the rectangles.  
+2. Visualize stock market. Stock market sectors can be represented as the top level and companies within sectors as sub-levels  
+3. News representation. It can be used to display news stories, categories as the top level and individual stories as sub-levels.  
+
+""")
 treemap()
 
 # Violin Plot using Seaborn
@@ -205,6 +317,17 @@ def violin_plot():
     plt.clf()
 
 st.header("Violin Plot Demo")
+st.markdown("""
+
+Violin plots combine aspects of box plots and kernel density plots, providing a detailed representation of the distribution of data.  
+  
+**Use Cases:**  
+1. Analyzing the distribution of customer satisfaction scores for different products  
+2. Visualizing the distribution of income levels for different demographics.  
+3. Comparing distribution of test scores for different schools.  
+4. Identifying outliers in data, such as customer purchase history.  
+
+""")
 violin_plot()
 
 # Word Cloud using WordCloud library
@@ -227,6 +350,17 @@ def word_cloud():
     plt.clf()
 
 st.header("Word Cloud Demo")
+st.markdown("""
+
+Word clouds are visual representations of text data where words are sized based on their frequency.  
+  
+**Use Cases:**  
+1. Social media analysis, such as social media posts, comments, and reviews to highlight what the customer sentiment is and identify trending topics.  
+2. Customer feedback analysis, it can be used to visualize customer feedback from surveys or reviews.  
+3. Content analysis, it can be used to analyze articles, reports, or website content.  
+4. SEO analysis, it can be used to identify the most frequent words used.  
+
+""")
 word_cloud()
 
 # 3D Surface Plot using Matplotlib
@@ -258,6 +392,18 @@ def surface_plot():
     plt.clf()
 
 st.header("3D Surface Plot Demo")
+st.markdown("""
+
+3D surface plots visualize three-dimensional data, illustrating how a response variable changes in relation to two predictor variables.  
+  
+**Use Cases:**  
+1. Terrain visualization, it can be used to create visual representation of terrain (mountains, hills, valleys, etc.)  
+2. Engineering design, it can be used to visualize the design of engineering structures (Bridges, Buildings, Airplanes, etc.).  
+3. Data analysis, it can be used to visualize data from scientific experiments or simulations.  
+4. Medical imaging, it can be used to create visualizations for medical images (MRI Scans, etc.).  
+5. Weather Forecasting, it can be used to visualize weather patterns (Temperature, Pressure, Wind Speed, etc.).  
+
+""")
 surface_plot()
 
 # Network Graph using NetworkX
@@ -287,6 +433,17 @@ def network_graph():
     plt.clf()
 
 st.header("Network Graph Demo")
+st.markdown("""
+
+### `14. Network Graphs`  
+Network graphs represent relationships between entities using nodes and edges. They are useful for visualizing connections in complex systems, such as social networks, transportation networks, or organizational structures.  
+  
+**Use Cases:**  
+1. Social Networks, can be used to analyze relationships (people, groups, and communities).  
+2. Transportation networks, can be used to display the transportation routes and connections.  
+3. Organizational Structures, can be used to visualize hierarchical relationships (organizations, rankings, etc.)  
+
+""")
 network_graph()
 
 # Sankey Diagram using Plotly
@@ -316,6 +473,17 @@ def sankey_diagram():
     return fig
 
 st.header("Sankey Diagram Demo")
+st.markdown("""
+
+Sankey diagrams visualize flow and quantity relationships between multiple entities. Often used in process engineering or energy flow analysis.  
+  
+**Use Cases:**  
+1. Track how energy is generated, distributed, and consumed in a system.  
+2. Show how users navigate through a website where they enter and exit.  
+3. Display the flow of money between different accounts or entities.  
+4. Show the movement of people or goods between different regions or countries.  
+
+""")
 fig = sankey_diagram()
 st.plotly_chart(fig)
 
@@ -339,14 +507,20 @@ df = pd.read_csv("datasets/tested.csv")
 
 st.write(df)
 
-st.write(df.info()) # Show the relevant information in the dataset such as Data Types
+from io import StringIO
+
+buffer = StringIO()
+df.info(buf=buffer)
+df_info_as_string = buffer.getvalue()
+
 st.write("Show the relevant information in the dataset such as Data Types")
+st.text(df_info_as_string) # Show the relevant information in the dataset such as Data Types
 
-st.write(df.isna().sum()) # Show the null values
 st.write("Show the null values")
+st.write(df.isna().sum()) # Show the null values
 
-st.write(df.describe()) # Generate Descriptive Statistics
 st.write("Generate Descriptive Statistics")
+st.write(df.describe()) # Generate Descriptive Statistics
 
 st.markdown("""
 
@@ -417,7 +591,7 @@ def pie_chart_passenger_based_on_sex():
     sex = df['Sex'].value_counts()
     colors = ['skyblue', 'pink']
     plt.pie(sex, labels = ['male', 'female'], autopct='%1.1f%%', colors=colors)
-    plt.title('Titanic Passenger Sex Distribution')
+    # plt.title('Titanic Passenger Sex Distribution')
     st.pyplot(plt)
     plt.clf()
     
@@ -431,7 +605,7 @@ def pie_chart_classes_distribution():
     survived = df['Pclass'].value_counts()
     colors = ['skyblue', 'lightgreen', 'salmon']
     plt.pie(survived, labels = ['3rd', '1st', '2nd'], autopct='%1.1f%%', colors=colors)
-    plt.title('Titanic Classes Distribution')
+    # plt.title('Titanic Classes Distribution')
     st.pyplot(plt)
     plt.clf()
     
@@ -444,7 +618,7 @@ def bar_plot_survival_based_on_sex():
     plt.figure(figsize=(7, 7)) # define the width and height of the graph
 
     ax = sns.countplot(x='Sex', hue='Survived', data=df)
-    plt.title('Passenger Sex vs. Survival')
+    # plt.title('Passenger Sex vs. Survival')
 
     # You can use this to show the actual count of each bars
     for p in ax.patches:
@@ -461,7 +635,7 @@ st.markdown("It is evident from the bar graph that **Female passengers had the h
 # Now we use a bar chart to show the survival rate of the passengers based on their class
 def bar_plot_survival_based_on_class():
     sns.countplot(x='Pclass', hue='Survived', data=df)
-    plt.title('Passenger Class vs. Survival')
+    # plt.title('Passenger Class vs. Survival')
     plt.xlabel('Passenger Class')
     plt.ylabel('Count')
     st.pyplot(plt)
@@ -484,7 +658,7 @@ We can see from the observations that the **third class** passengers had the **h
 # Let's check how the Age of the passengers are distributed using Histogram
 def histogram_age_distribution():
     sns.histplot(df['Age'], bins=30, color="salmon")
-    plt.title('Passenger Age Distribution')
+    # plt.title('Passenger Age Distribution')
     plt.xlabel('Passenger Age')
     plt.ylabel('Count')
     st.pyplot(plt)
